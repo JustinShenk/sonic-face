e743c94e8bcefa145727c57ab28cfe8d0d4d6748
junbohuang <junbo.huang.sz@gmail.com>
2017-05-30 17:30:31 +0200

    normalize data for each frame and each axis(x,y)

--- normalize_data.py
+++ normalize_data.py
@@ -1,10 +1,11 @@
-# for each frame(1-10) and each axis(x and y), do normalizing
+import numpy as np
+from sklearn import preprocessing as pp
 
+print('data normalized')
+#for each frame(1-10) and each axis(x and y), do normalizing
 def normalize_data(data):
-    normalized = np.empty([10,40,40,2])
     for i in range(len(data)):
         for n in range(len(data[0][0][0])):
-            normalized[i,:,:,n]=pp.normalize(data[i,:,:,n])
-            data = normalized
+            data[i,:,:,n]=pp.normalize(data[i,:,:,n])
     return data
 
