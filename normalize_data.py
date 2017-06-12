ca321b449f16d966bccf3d30680819b6dafa00bc
Justin Shenk <shenkjustin@gmail.com>
2017-06-12 12:43:03 +0200

    Replace integer indices with ellipsis

--- normalize_data.py
+++ normalize_data.py
@@ -14,12 +14,12 @@ def normalize_data(data):
     # get max/min of x/y across samples and frames
     for gesture in gestures:
         data_gesture = np.asarray(data[gesture])
-        max_x = np.max(data_gesture[:,:,:,:,0])
-        min_x = np.min(data_gesture[:,:,:,:,0])
-        max_y = np.max(data_gesture[:,:,:,:,1])
-        min_y = np.min(data_gesture[:,:,:,:,1])
-        data_gesture[:,:,:,:,0]=(data_gesture[:,:,:,:,0]-min_x)/(max_x - min_x)
-        data_gesture[:,:,:,:,1]=(data_gesture[:,:,:,:,1]-min_y)/(max_y - min_y)
+        max_x = np.max(data_gesture[...,0])
+        min_x = np.min(data_gesture[...,0])
+        max_y = np.max(data_gesture[...,1])
+        min_y = np.min(data_gesture[...,1])
+        data_gesture[...,0]=(data_gesture[...,0]-min_x)/(max_x - min_x)
+        data_gesture[...,1]=(data_gesture[...,1]-min_y)/(max_y - min_y)
 
         #store normalized data into dictionary
         gdata[gesture] = data_gesture
