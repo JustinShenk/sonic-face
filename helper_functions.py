9a10825585f3ec98439af58723ad39d5939ecf52
Justin Shenk <shenkjustin@gmail.com>
2017-07-03 17:03:30 +0200

    Add report function for random search output

--- helper_functions.py
+++ helper_functions.py
@@ -580,3 +580,22 @@ def get_data_list(divs=[4, 10, 20]):
         data_list.append(data)
     print("Data loaded")
     return data_list
+
+
+def report(results, n_top=3):
+    """Utility function to report best scores.
+    Source: Blatantly ripped from scikit-learn.org.
+
+    Args:
+        results: RandomizedSearchCV object
+        n_top: int
+    """
+    for i in range(1, n_top + 1):
+        candidates = np.flatnonzero(results['rank_test_score'] == i)
+        for candidate in candidates:
+            print("Model with rank: {0}".format(i))
+            print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+                  results['mean_test_score'][candidate],
+                  results['std_test_score'][candidate]))
+            print("Parameters: {0}".format(results['params'][candidate]))
+            print("")
